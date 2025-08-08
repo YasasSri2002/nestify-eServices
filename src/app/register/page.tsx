a045309819991b5477b59cc03c5bbc1d584e12fd
YasasSri2002 <srimedagedara234@gmail.com>
2025-08-08 23:21:07 +0530

    register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,46 @@
-import RegisterForm from "@/components/ui/registerForm";
-
+"use client";
+import { useRef } from "react";
 export default function RegisterPage(){
+
+    const checkedRef= useRef(false);
+
+    const handleClick = () =>{
+       const el = document.getElementById("providerRadio") as HTMLInputElement | null;
+        if (el) {
+            el.checked = true;
+        }
+        
+        console.log(el?.value);
+        console.log(checkedRef);
+    }
+
     return(
         <>
-            <RegisterForm/>
+            <div className="flex justify-center">
+                 <div className="m-15 w-full bg-red-200 h-fit rounded-4xl p-5 grid
+                grid-cols-2 space-x-6">
+                    <div className="col-1 grid justify-items-center">
+                        
+                    </div>
+                    <div className="col-2 grid  justify-items-center">
+                        <div className="grid">
+                            <div className="flex">
+                                <input type="radio" name="provider" id="providerRadio" 
+                                className="sr-only"
+                                checked = {false}
+                                value="provider"
+                                onChange={(e)=> checkedRef.current = e.target.checked}
+                                
+                                    />
+                                    <label htmlFor="provider" onClick={handleClick}>fk</label>
+                            </div>
+
+                            <input type="radio" name="client" id="clientRadio" />
+                        </div>
+                    </div>
+
+                </div> 
+               </div>
         </>
     );
 } 
--- src/components/ui/registerForm.tsx
+++ src/components/ui/registerForm.tsx
@@ -3,16 +3,7 @@
 export default function registerForm(){
     return(
             <>
-                <div>
-                    <div className="flex justify-center content-center">
-                        <div className="w-1/2 flex-1 justify-center flex bg-red-200">
-                            Provider
-                        </div>
-                        <div className="w-1/2 flex-1 justify-center flex bg-blue-200">
-                            client
-                        </div>
-                    </div>  
-                </div> 
+               
             </>
     );
 }
