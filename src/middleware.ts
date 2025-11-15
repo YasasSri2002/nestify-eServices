5f785b0438d95a242ac11f66b850e485d8269561
YasasSri2002 <srimedagedara234@gmail.com>
2025-11-15 13:35:21 +0530

    extend middleware to handle user provider as well 3 protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,44 +2,89 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import jwt from 'jsonwebtoken';
+import { NextURL } from 'next/dist/server/web/next-url';
+import next from 'next';
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('auth-token')?.value;
+  const {pathname} = request.nextUrl;
 
-  // Only protect /site-admin routes
-  if (request.nextUrl.pathname.startsWith('/site-admin')) {
-    // If no token, redirect to Keycloak login
+  const protectedRoutes =[
+    '/site-admin',
+    '/user/profile',
+    'provider/profile',
+  ]
+
+  //check if current url is in the protected route
+  const isProtectedUrl = protectedRoutes.some(route=>pathname.startsWith(route));
+
+  if(isProtectedUrl){
+
     if (!token) {
       const loginUrl = process.env.NEXT_PUBLIC_LOGIN_URL;
-      return NextResponse.redirect(loginUrl!);
+      
+      const redirectUrl = new URL(loginUrl!)
+      redirectUrl.searchParams.set('redirect',pathname);
+      return NextResponse.redirect(redirectUrl);
     }
 
-    try {
-      console.log(token)
-      const decoded: any = jwt.decode(token);
+    const decoded: any = jwt.decode(token);
+    
+  if (pathname.startsWith('/site-admin')) {
 
-      // Adjust this depending on where roles are in your token
-      const isAdmin = decoded?.realm_access?.roles?.includes('admin');
-      console.log(decoded?.roles);
+        try {
+          //check if user is a admin
+          const isAdmin = decoded?.realm_access?.roles?.includes('admin');
 
-      if (!isAdmin) {
-        console.log("not an admin");
-        // Redirect non-admin users to home or an error page
-        return NextResponse.redirect(new URL('/', request.url));
-        
-      }
-    } catch (err) {
-      console.error('Token decode error:', err);
-      return NextResponse.redirect(new URL('/', request.url));
+          if (!isAdmin) {           
+            // Redirect non-admin users to home or an error page
+            //TODO: make a forbidden page 403 Error page
+            return NextResponse.redirect(new URL('/', request.url));
+            
+          }
+
+        } catch (err) {
+          console.error('Token decode error:', err);
+          return NextResponse.redirect(new URL('/', request.url));
+        }
     }
+
+    if(pathname.startsWith('user/profile')){
+        try{
+          const isUser = decoded?.realm_access?.roles?.includes('user','admin');
+          if(!isUser){
+            //TODO: make a 403 forbidden Page
+            return NextResponse.redirect(new URL('/',request.url));
+          }
+
+        }catch(err){
+            console.log(err);
+            return NextResponse.redirect(new URL('/', request.url));
+        }
+    }
+
+    if(pathname.startsWith('provider')){
+        try{
+          const isProvider = decoded?.realm_access?.roles?.includes('provider','admin');
+          if(!isProvider){
+            //TODO: make a 403 forbidden page
+            return NextResponse.redirect(new URL('/',request.url));
+          }
+        }catch(err){
+          console.log(err);
+          return NextResponse.redirect(new URL('/',request.url))
+        }
+    }
+
   }
-
-  
-
   // Allow /login-callback and other routes
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/site-admin/:path*'], 
+  matcher: [
+      '/site-admin/:path*',
+      '/user/profile/:path*',
+      '/provider/profile/:path*'
+  ], 
 };
