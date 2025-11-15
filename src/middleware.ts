d7b7a5040ccc83866a5129c96820935a69b112b8
YasasSri2002 <srimedagedara234@gmail.com>
2025-11-15 18:13:04 +0530

    middleware.ts made more readable got rid the nested if statements

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,81 +2,81 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import jwt from 'jsonwebtoken';
-import { NextURL } from 'next/dist/server/web/next-url';
-import next from 'next';
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get('auth-token')?.value;
-  const {pathname} = request.nextUrl;
-
-  const protectedRoutes =[
+ const protectedRoutes =[
     '/site-admin',
     '/user/profile',
-    'provider/profile',
+    '/provider/profile',
   ]
 
-  //check if current url is in the protected route
-  const isProtectedUrl = protectedRoutes.some(route=>pathname.startsWith(route));
-
-  if(isProtectedUrl){
-
-    if (!token) {
-      const loginUrl = process.env.NEXT_PUBLIC_LOGIN_URL;
-      
-      const redirectUrl = new URL(loginUrl!)
-      redirectUrl.searchParams.set('redirect',pathname);
-      return NextResponse.redirect(redirectUrl);
-    }
-
-    const decoded: any = jwt.decode(token);
-    
-  if (pathname.startsWith('/site-admin')) {
-
-        try {
-          //check if user is a admin
-          const isAdmin = decoded?.realm_access?.roles?.includes('admin');
-
-          if (!isAdmin) {           
-            // Redirect non-admin users to home or an error page
-            //TODO: make a forbidden page 403 Error page
-            return NextResponse.redirect(new URL('/', request.url));
-            
-          }
-
-        } catch (err) {
-          console.error('Token decode error:', err);
-          return NextResponse.redirect(new URL('/', request.url));
-        }
-    }
-
-    if(pathname.startsWith('user/profile')){
-        try{
-          const isUser = decoded?.realm_access?.roles?.includes('user','admin');
-          if(!isUser){
-            //TODO: make a 403 forbidden Page
-            return NextResponse.redirect(new URL('/',request.url));
-          }
-
-        }catch(err){
-            console.log(err);
-            return NextResponse.redirect(new URL('/', request.url));
-        }
-    }
-
-    if(pathname.startsWith('provider')){
-        try{
-          const isProvider = decoded?.realm_access?.roles?.includes('provider','admin');
-          if(!isProvider){
-            //TODO: make a 403 forbidden page
-            return NextResponse.redirect(new URL('/',request.url));
-          }
-        }catch(err){
-          console.log(err);
-          return NextResponse.redirect(new URL('/',request.url))
-        }
-    }
-
+function hasRole(jwtDecoded: any , roles: string[]){
+    const accessRoles = jwtDecoded?.realm_access?.roles || [];
+    return roles.some(role => accessRoles?.includes(role));
   }
+
+function validateToken(token: any){
+  if(!token) return null;
+
+  try{
+    return jwt.decode(token);
+  }catch(err:any){
+    console.log("token error",err)
+    return null;
+  }
+ 
+}
+
+function checkifProtectedUrl(request: NextRequest, protectedRoutes: string[]){
+  const {pathname} = request.nextUrl;
+  return protectedRoutes.some(route=>pathname.startsWith(route));
+
+}
+
+function handleUnauthorizationEntries(pathname: any){
+      const loginUrl = process.env.NEXT_PUBLIC_LOGIN_URL;
+      const redirectUrl = new URL(loginUrl!);
+      redirectUrl.searchParams.set('redirect', pathname);
+      return NextResponse.redirect(redirectUrl);
+}
+
+function authorizationForRoutes(pathname: any , decodedToken:any){
+   if(pathname.startsWith('/user/profile')){
+          return  hasRole(decodedToken,['user','admin']);
+    }
+    if(pathname.startsWith('/site-admin')){
+      return  hasRole(decodedToken,['admin']);
+    }
+    if(pathname.startsWith('/provider/profile')){
+      return  hasRole(decodedToken,['provider','admin']);
+    }
+    return true;
+}
+
+
+
+export function middleware(request: NextRequest) {
+
+  const token = request.cookies.get('auth-token')?.value;
+
+  const {pathname} = request.nextUrl;
+
+  const decoded: any = validateToken(token);
+
+  //check if current url is in the protected route
+  if(!checkifProtectedUrl(request,protectedRoutes)){
+    return NextResponse.next();
+  }
+  
+  if(!token){
+      return handleUnauthorizationEntries(pathname);      
+    }
+
+    if(!authorizationForRoutes(pathname,decoded)){
+      //TODO: make a forbidden 403 page
+      return NextResponse.redirect(new URL('/',request.url));
+    }
+
+
   // Allow /login-callback and other routes
   return NextResponse.next();
 }
