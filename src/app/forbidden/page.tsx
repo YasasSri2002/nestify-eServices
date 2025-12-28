ce1f3438776619f4be712878c0246bbcb0b03e05
YasasSri2002 <srimedagedara234@gmail.com>
2025-12-28 15:17:49 +0530

    forbidden page changes according to the es lint

--- src/app/forbidden/page.tsx
+++ src/app/forbidden/page.tsx
@@ -1,4 +1,5 @@
 import DynamicIcon from "@/components/utill/DynamicIcons";
+import Image from "next/image";
 import Link from "next/link";
 export default function Forbidden(){
     return(
@@ -6,10 +7,15 @@ export default function Forbidden(){
                 <div className="text-center w-full h-full grid justify-items-center content-center text-gray-500/90">
                     <h1 className="text-2xl md:text-6xl text-gray-500 text-center">Error - 403</h1>
                     <div className="px-10 py-2  md:p-0">
-                        <img src="/icon/403.jpg" alt="403 door is guarded" className="md:w-100 md:h-100 relative -z-10 rounded-2xl" />
+                        <Image
+                             src="/icon/403.jpg" alt="403 door is guarded" 
+                             className="md:w-100 md:h-100 relative  rounded-2xl"
+                             width={100}
+                             height={100}
+                            />
                     </div>
                     <h1 className="md:text-2xl text-black text-center">Access Denied!</h1>
-                    <h1 className="md:text-xl"> You don't have permission to acess this page </h1>
+                    <h1 className="md:text-xl"> You don&apos;t have permission to acess this page </h1>
                     <h1 className="text-center md:text-xl">please make sure you have right aceess</h1>
                     
                         <Link href="/" className="flex text-xl md:text-2xl text-black/90 items-center justify-center hover:text-blue-600">
