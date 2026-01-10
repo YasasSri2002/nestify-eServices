3c7d77d2053c00d7a9c3c571890886c38485c1b7
YasasSri2002 <srimedagedara234@gmail.com>
2026-01-10 17:12:17 +0530

    api/ redo of get all review api

--- src/app/api-calls/reviews/route.ts
+++ src/app/api-calls/reviews/route.ts
@@ -1,13 +1,17 @@
 
 import { ReviewDto } from "@/dto/ReviewDto";
 
-
-const API_PREFIX = "/api-calls/auth/apis";
 const SPRING_BOOT_URL = process.env.SPRING_BOOT_API_URL;
 
 export async function getReviewsByGigId(id: string): Promise<ReviewDto[]>{
     
-        const response =  await fetch(`http://localhost:3000/${API_PREFIX}/api/v1/review/by-gig-id?id=${id}`)
+        const response =  await fetch(`${SPRING_BOOT_URL}/api/v1/review/by-gig-id?id=${id}`,{
+            method: "GET",
+            next:{
+                revalidate: 300,
+                tags: ['reviews']
+            }
+        })
 
         if(!response.ok){
             throw (response.status); 
