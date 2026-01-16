c84fd4b729ff24ae931b873f77b48797fb76af83
YasasSri2002 <srimedagedara234@gmail.com>
2026-01-16 16:34:12 +0530

    fix/ get all providers with suspense

--- src/app/api-calls/provider/all/route.ts
+++ src/app/api-calls/provider/all/route.ts
@@ -1,11 +1,19 @@
 
 import { ProviderDto } from "@/dto/ProviderDto";
 
-const SPRING_BOOT_URL = process.env.SPRING_BOOT_API_URL;
+const SPRING_BOOT_URL = process.env.SPRING_BOOT_API_URL || 'http://localhost:8080';
 
 export async function getAllProviders():Promise<ProviderDto[]>{
+
+    if(!SPRING_BOOT_URL){
+        console.error("backed is not up --> get providers All")
+    }
+
     const response = await fetch(`${SPRING_BOOT_URL}/api/v1/providers/all`,{
-        cache: 'no-store'
+        next: {
+            revalidate: 7200,
+            tags: ['providers']
+        }
     });
 
     if (!response.ok) {
---
+++ src/components/ui/provider-section/allProvidersPage.tsx
@@ -0,0 +1,97 @@
+'use client'
+import { useSearchParams } from "next/navigation"; // Add this import
+import {useEffect, useState } from "react";
+
+import { ProviderDto } from "@/dto/ProviderDto";
+import BookingProvidersCard  from "@/components/ui/provider-section/bookingProvidersCard";
+import DynamicIcon from "@/components/utill/DynamicIcons";
+import PaginationControls from "@/components/utill/paginationControls";
+import { getAllProviders } from "@/app/api-calls/provider/all/route";
+import {FullPageLoading} from "@/components/utill/loadingPage";
+  
+export default function AllProvidersPage(){
+
+    const images =[
+  "https://avatar.iran.liara.run/public/girl",
+  "https://avatar.iran.liara.run/public/boy",
+  "https://avatar.iran.liara.run/public/boy?username=Scott",
+  "https://avatar.iran.liara.run/public/boy?username=Maria",
+  "https://avatar.iran.liara.run/username?username=nayanja+nipunsara",
+  "https://avatar.iran.liara.run/username?username=[firstname+lastname]",
+  "https://avatar.iran.liara.run/username?username=[firstname+lastname]",
+  "https://avatar.iran.liara.run/username?username=[firstname+lastname]"
+  ]
+
+  const [providers, setProviders] = useState<ProviderDto[]>([])
+  const [loadProviders,setLoadProviders] =useState(true);
+
+  useEffect(()=>{
+    async function fetchProivders(){
+      try{
+          const response = await getAllProviders();
+          setProviders(response);
+          setLoadProviders(false);
+          console.log(response)
+      }catch(err:any){
+          console.log(err)
+      }
+    }
+    fetchProivders();
+
+  },[])
+
+    const searchParams = useSearchParams(); // Using client-side hook
+    const page = searchParams.get('page') ?? '1';
+    const per_page = searchParams.get('per_page') ?? '5';
+  
+    const start = (Number(page) - 1) * Number(per_page);
+    const end = start + Number(per_page);
+    const entries = providers.slice(start, end);
+
+    if(loadProviders) return <FullPageLoading/>
+
+
+    return(
+        <div className="grid justify-items-center sm:justify-items-normal">
+        <div className="sm:grid sm:grid-cols-4">
+        <div className="sm:col-1 bg-gradient-to-t from-white from-10% via-gray-100 via-30% to-gray-200 to-90% w-dvw sm:w-full h-[10em] sm:h-full rounded-b-2xl ">
+          <div className="sm:mx-6 my-2 pt-5">
+            <h1>Search service providers</h1>
+          </div>
+          <div className="flex justify-center sm:justify-normal items-center space-x-2">
+            <DynamicIcon name="CiSearch" className="relative left-10" />
+            <input
+              type="text"
+              placeholder="search"
+              className="bg-white rounded-2xl px-10 w-40 md:w-50 lg:w-fit"
+            />
+          </div>
+          
+        </div>
+        <div className="sm:col-span-3 bg-white w-dvw sm:w-full  p-10">
+          <div className="grid justify-items-center w-full gap-5">
+            {
+              entries.map((provider, index)=>(
+                <BookingProvidersCard key={provider.email} 
+                providers={provider}
+                images={images[index % images.length]}
+                />
+              ))}
+              
+          </div>
+        </div>
+
+        {/* Moved pagination to inside content area */}
+        <div className="grid col-span-4 justify-items-center">
+          <PaginationControls
+            hasNextPage={end < providers.length}
+            hasPrevPage={start > 0}
+            endPage={providers.length}
+            perPageNumber='5'
+            routerPath="providers"
+          />
+        </div>
+      </div>
+      </div>
+    )
+}
