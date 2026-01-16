9c664f425bdf80e1c3dfe02a0394affa3a3000cf
YasasSri2002 <srimedagedara234@gmail.com>
2026-01-16 18:43:57 +0530

    cache the provider get all api

--- src/app/api-calls/provider/all/route.ts
+++ src/app/api-calls/provider/all/route.ts
@@ -1,17 +1,18 @@
-
+'use server'
 import { ProviderDto } from "@/dto/ProviderDto";
+import { cacheLife, cacheTag } from "next/cache";
 
 const SPRING_BOOT_URL = process.env.SPRING_BOOT_API_URL || 'http://localhost:8080';
 
 export async function getAllProviders():Promise<ProviderDto[]>{
-
+'use cache'
     if(!SPRING_BOOT_URL){
         console.error("backed is not up --> get providers All")
     }
 
     const response = await fetch(`${SPRING_BOOT_URL}/api/v1/providers/all`,{
         next: {
-            revalidate: 7200,
+            revalidate: 24,
             tags: ['providers']
         }
     });
@@ -21,6 +22,8 @@ export async function getAllProviders():Promise<ProviderDto[]>{
         throw new Error(error.error || 'Failed to fetch providers');
   }
 
+  cacheTag('providers')
+  cacheLife('hours');
     return response.json();
 }
 
