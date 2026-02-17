4465abfa727b4dd19c69da8d83f3ff5f536cb9ce
YasasSri2002 <srimedagedara234@gmail.com>
2026-02-17 14:29:00 +0530

    updated/two factor details page

--- src/app/users/profile/twofactor.tsx
+++ src/app/users/profile/twofactor.tsx
@@ -10,15 +10,23 @@ export default function TwoFactorDetails(){
                     <div className="grid gap-1">
                         <h1>Two Factor Authentication</h1>
                         <h3 className="text-gray-600">Protect your Account with 2FA</h3>
+                        <button className="bg-slate-950 text-white rounded-md px-4 py-1 my-2 active:bg-white 
+                            active:text-slate-950">
+                            Enable Two Factor
+                        </button>
                     </div>
-                    <button className="border border-red-500 text-red-500 px-4 text-sm h-6 rounded-md">Not Activated</button>
+                    <label className="border border-red-500 text-red-500 px-4 text-sm h-6 rounded-md">Not Activated</label>
                 </div>
                 <div className="flex justify-between items-center">
                     <div className="grid gap-1">
                         <h1>Add Authenticator App</h1>
                         <h3 className="text-gray-600">Protect your Account with Authenticator app</h3>
+                        <button className="bg-slate-950 text-white rounded-md px-4 py-1 my-2 active:bg-white 
+                            active:text-slate-950 active:scale-75 active:duration-300 transition-all ">
+                            Set up authentication app
+                        </button>
                     </div>
-                    <button className="border border-red-500 text-red-500 px-4 text-sm h-6 rounded-md">Set up</button>
+                    <label className="border border-red-500 text-red-500 px-4 text-sm h-6 rounded-md">Not Activated</label>
                 </div>
 
             </div>
