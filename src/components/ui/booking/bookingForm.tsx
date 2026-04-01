8199aea60aff5fe476b7cc5f06155895acb77a5c
YasasSri2002 <srimedagedara234@gmail.com>
2026-04-01 12:08:06 +0530

    refactor: booking form

--- src/components/ui/booking/bookingForm.tsx
+++ src/components/ui/booking/bookingForm.tsx
@@ -1,49 +1,80 @@
 "use client"
-import { FormEvent } from "react";
 
-async function onSubmit(event: FormEvent<HTMLFormElement>){
+import { FormEvent } from "react";
+import DynamicIcon from "@/components/utill/DynamicIcons";
+
+
+
+export default function BookingForm(
+    {onClose,onSubmit}: {onClose: () => void , onSubmit: (bookingRequestDto: any) => void }){
+
+    async function onFormSubmit(event: FormEvent<HTMLFormElement>){
         event.preventDefault()
         const formData = new FormData(event.currentTarget)
         console.log(formData);
+
+        onSubmit(formData);
     }
 
-export default function BookingForm(){
-
     return(
-        <div className="border-1 xl:w-200 rounded-2xl px-10">
-            <h1 className="text-center" >Booking Information</h1>
-            <form onSubmit={onSubmit} className="w-full gap-5 grid">
-                <div className="grid">
-                    <label htmlFor="name">Full name</label>
-                    <input type="text" name="name" placeholder="Enter your name" className="border-[0.5px] h-8 rounded-sm outline-none pl-2"/>
+        <div className="fixed inset-0 flex items-center justify-center w-full z-50"
+            onClick={onClose}
+        >
+            <main className="max-h-[90vh] max-w-5xl grid bg-white rounded-xl shadow-xl overflow-y-auto mx-4 p-5 gap-10  ">
+                <div className="flex justify-end">
+                    <button onClick={onClose} >
+                        <DynamicIcon name="IoClose" className="w-5 h-5"/>
+                    </button>
                 </div>
-                <div className="flex justify-between">
-                <div className="grid ">
-                    <label htmlFor="email">Email address</label>
-                    <input type="email" name="email" placeholder="Enter your email" className="border-[0.5px] h-8 rounded-sm outline-none pl-2 w-[20em]"/>
-                </div>
-                <div className="grid">
-                    <label htmlFor="contact">Contact number</label>
-                    <input type="text" name="contact" placeholder="Enter your contact number" className="border-[0.5px] h-8 rounded-sm outline-none pl-2 w-[20em]"/>
-                </div>
-                </div>
-                <div className="grid">
-                    <label htmlFor="address">Address</label>
-                    <input type="text" name="address" placeholder="Enter your address" className="border-[0.5px] h-8 rounded-sm outline-none pl-2"/>
-                </div>
-                <div className="flex justify-between">
-                <div className="grid ">
-                    <label htmlFor="booking-date">Date:</label>
-                    <input type="date" defaultValue={"2026-07-22"} name="booking-date" className="w-[17em] h-8 rounded-sm text-center bg-gray-200"/>
-                </div>
-                <div className="grid">
-                    <label htmlFor="starting-time">Starting time:</label>
-                    <input type="time" defaultValue={"07:00"} name="starting-time" className="w-[17em] h-8 rounded-sm  text-center bg-gray-200"/>
-                </div>
-                </div>
-                
-                    <button type="submit" className="bg-gray-200  rounded-sm h-10">Submit</button>
-            </form>
+                <header className="grid gap-5">
+                    <div className="flex item-center justify-center w-full">
+                        <DynamicIcon name="FaCalendarCheck" className="w-10 h-10"></DynamicIcon>
+                    </div>
+                    <h1 className="text-center text-lg md:text-xl lg:text-2xl" >Booking Information</h1>
+                </header>
+                <form onSubmit={onFormSubmit} className="w-full gap-5 grid">
+                    <div className="flex justify-between gap-10">
+                        <div className="grid flex-1">
+                            <label htmlFor="firstName">First name</label>
+                            <input type="text" name="firstName" placeholder="Enter your name" className="border-[0.5px] h-8 rounded-sm outline-none pl-2"/>
+                        </div>
+                        <div className="grid flex-1">
+                            <label htmlFor="lastName">Last name</label>
+                            <input type="text" name="lastName" placeholder="Enter your name" className="border-[0.5px] h-8 rounded-sm outline-none pl-2"/>
+                        </div>
+                    </div>
+                    <div className="flex justify-between gap-10">
+                    <div className="grid flex-1 ">
+                        <label htmlFor="email">Email address</label>
+                        <input type="email" name="email" placeholder="Enter your email" 
+                            className="border-[0.5px] h-8 rounded-sm outline-none pl-2 "/>
+                    </div>
+                    <div className="grid flex-1">
+                        <label htmlFor="contact">Contact number</label>
+                        <input type="text" name="contact" placeholder="Enter your contact number" 
+                        className="border-[0.5px] h-8 rounded-sm outline-none pl-2 "/>
+                    </div>
+                    </div>
+                    <div className="grid">
+                        <label htmlFor="address">Address</label>
+                        <input type="text" name="address" placeholder="Enter your address" className="border-[0.5px] h-8 rounded-sm outline-none pl-2"/>
+                    </div>
+                    <div className="flex justify-between gap-10">
+                    <div className="grid ">
+                        <label htmlFor="booking-date">Starting Date</label>
+                        <input type="date" defaultValue={"2026-07-22"} name="booking-date" 
+                        className="w-[17em] h-8 rounded-sm text-center bg-gray-200"/>
+                    </div>
+                    <div className="grid">
+                        <label htmlFor="starting-time">Starting time</label>
+                        <input type="time" defaultValue={"07:00"} name="starting-time" 
+                        className="w-[17em] h-8 rounded-sm  text-center bg-gray-200"/>
+                    </div>
+                    </div>
+                    
+                        <button type="submit" className="bg-gray-200  rounded-sm h-10">Submit</button>
+                </form>
+            </main>
         </div>
     );
 }
--- src/components/ui/service-gig/fullServiceDetails.tsx
+++ src/components/ui/service-gig/fullServiceDetails.tsx
@@ -4,110 +4,132 @@ import Image from "next/image";
 
 import DynamicIcon from "@/components/utill/DynamicIcons";
 import { ServiceGigResponseDto } from "@/dto/response/ServiceGigResponseDto";
+import { useState } from "react";
+import BookingForm from "../booking/bookingForm";
 
 function showProviderDetails(){
         const providerDetailsPanel = document.getElementById(`providerDetails`);
         providerDetailsPanel?.classList.toggle('sr-only')
     }
 
-export default function FullServiceGigsDetails({gig}:
-     {readonly gig: ServiceGigResponseDto})
+export default function FullServiceGigsDetails({gig}:{readonly gig: ServiceGigResponseDto}){
 
-     {
+    const [showBookingForm,setShowBookingForm] = useState(false);
 
     return(
-        <div className="grid grid-cols-6 bg-linear-to-br to-gray-200 from-gray-300 via-blue-50">
-            {/* mobile-menue */}
-            <div className="h-full sm:hidden bg-gray-300 grid justify-items-center content-between py-2">
-                <button onClick={showProviderDetails}>
-                    <DynamicIcon name="BiMenu" className="text-3xl"/>
-                </button>
-                <div className="w-10 h-10 rounded-full">
-                    <Image src={"https://avatar.iran.liara.run/public/boy"} 
-                        width={100}
-                        height={100}
-                        alt="provider's profile picture"/>
-                </div>
-            </div>
-
-            {/* left-side */}
-
-           <div id="providerDetails" className="col-span-2 grid justify-items-center order-2 
-                    sm:order-1 absolute bg-gray-300 sm:bg-white/5 z-50 sr-only sm:not-sr-only gap-5 sm:p-4 h-full
-                    ">
-                <div className="w-full flex justify-end sm:sr-only ">
-                    <button onClick={showProviderDetails}>
-                        <DynamicIcon name="IoMdClose" className="text-xl"/>
-                    </button>
-                </div>
-                <div className="w-30 h-30 rounded-full">
-                    <Image src={"https://avatar.iran.liara.run/public/boy"} 
-                        width={100}
-                        height={100}
-                        alt="provider's profile picture"/>
-                </div>
-                <div className="grid gap-5">
-                    <div className="flex space-x-4 justify-center">
-                        <h1 className="md:text-xl">
-                            Get to know<span className="capitalize"> {gig.provider.userName}</span>
-                        </h1>
-                        {
-                            gig.provider.isVerified ? 
-                            <DynamicIcon name="BiBadgeCheck" className="text-green-400 text-2xl" /> : ""
-                        }
-                    </div>
-                    <p>{gig.provider.shortDescription}</p>
-                </div>
-                <div className="grid xl:grid-cols-2 ">
-                    <div className="xl:col-1">
-                        <h1>Experience:{gig.provider.experience}</h1>
-                        <h1>Main Category: {gig.provider.expertise}</h1>
-                        <h1>Other Categories: </h1>
-                    </div>
-                    <div className="xl:col-2 gap-2 grid">
-                        <h1>Up to date {gig.provider.jobCount} jobs has successfully compeleted</h1>
-                        
-                        <div className="grid justify-items-center w-fit gap-2">
-                            <h1>Contacts</h1>
-                            <div className="flex justify-between w-full lg:">
-                                <h1>Phone:</h1> <h1>{gig.provider.contactNo}</h1>
-                            </div>
-                             <div className="flex justify-between w-full lg:">
-                                <h1>Email:</h1> <h1>{gig.provider.email}</h1>
-                            </div>
+        <>
+            <main>
+                <div className="grid grid-cols-6 bg-linear-to-br to-gray-200 from-gray-300 via-blue-50">
+                    {/* mobile-menue */}
+                    <div className="h-full sm:hidden bg-gray-300 grid justify-items-center content-between py-2">
+                        <button onClick={showProviderDetails}>
+                            <DynamicIcon name="BiMenu" className="text-3xl"/>
+                        </button>
+                        <div className="w-10 h-10 rounded-full">
+                            <Image src={"https://avatar.iran.liara.run/public/boy"} 
+                                width={100}
+                                height={100}
+                                alt="provider's profile picture"/>
                         </div>
                     </div>
-                    
-                </div>
-           </div>
-        
-           {/* right-side */}
-           <div className="col-span-5 sm:col-span-4  sm:order-2 p-5 ">
-                <h1 className="lg:text-2xl text-center capitalize mt-2 mb-10 ">{gig.title}</h1>
-                <div className="grid gap-5 md:grid-cols-2">
-                       <div className="md:col-1 grid gap-4 ">
-                             <div className="grid gap-2">
-                                <h1 className="lg:text-2xl ">About this gig</h1>
-                                <p className="text-gray-600 md:text-lg">{gig.fullDescription}</p>
+
+                    {/* left-side */}
+
+                <div id="providerDetails" className="col-span-2 grid justify-items-center order-2 
+                            sm:order-1 absolute bg-gray-300 sm:bg-white/5 z-50 sr-only sm:not-sr-only gap-5 sm:p-4 h-full
+                            ">
+                        <div className="w-full flex justify-end sm:sr-only ">
+                            <button onClick={showProviderDetails}>
+                                <DynamicIcon name="IoMdClose" className="text-xl"/>
+                            </button>
+                        </div>
+                        <div className="w-30 h-30 rounded-full">
+                            <Image src={"https://avatar.iran.liara.run/public/boy"} 
+                                width={100}
+                                height={100}
+                                alt="provider's profile picture"/>
+                        </div>
+                        <div className="grid gap-5">
+                            <div className="flex space-x-4 justify-center">
+                                <h1 className="md:text-xl">
+                                    Get to know<span className="capitalize"> {gig.provider.userName}</span>
+                                </h1>
+                                {
+                                    gig.provider.isVerified ? 
+                                    <DynamicIcon name="BiBadgeCheck" className="text-green-400 text-2xl" /> : ""
+                                }
                             </div>
-                            
-                        </div> 
-                        <div className="md:col-2 sticky grid content-center h-full ">
-                            <div className="bg-gray-300 p-5 rounded-2xl">
-                                <h1 className="lg:text-2xl">Abour Prices</h1>
-                                <div className="ml-3">
-                                    <h1>The base price: {gig.basePrice} {gig.currency}</h1>
-                                    <h1>How price adding: {gig.priceType}</h1>
+                            <p>{gig.provider.shortDescription}</p>
+                        </div>
+                        <div className="grid xl:grid-cols-2 ">
+                            <div className="xl:col-1">
+                                <h1>Experience:{gig.provider.experience}</h1>
+                                <h1>Main Category: {gig.provider.expertise}</h1>
+                                <h1>Other Categories: </h1>
+                            </div>
+                            <div className="xl:col-2 gap-2 grid">
+                                <h1>Up to date {gig.provider.jobCount} jobs has successfully compeleted</h1>
+                                
+                                <div className="grid justify-items-center w-fit gap-2">
+                                    <h1>Contacts</h1>
+                                    <div className="flex justify-between w-full lg:">
+                                        <h1>Phone:</h1> <h1>{gig.provider.contactNo}</h1>
+                                    </div>
+                                    <div className="flex justify-between w-full lg:">
+                                        <h1>Email:</h1> <h1>{gig.provider.email}</h1>
+                                    </div>
                                 </div>
                             </div>
-                        </div>  
+                            
+                        </div>
                 </div>
-                <div className="flex space-x-5 w-full items-center  my-10">
-                    <h1>Category: </h1>  
-                    <h1 className="bg-gray-200 rounded-xl px-4 py-1">{gig.category.name}</h1>
+                
+                {/* right-side */}
+                <div className="col-span-5 sm:col-span-4  sm:order-2 p-5 ">
+                        <h1 className="lg:text-2xl text-center capitalize mt-2 mb-10 ">{gig.title}</h1>
+                        <div className="grid gap-5 md:grid-cols-2">
+                            <div className="md:col-1 grid gap-4 ">
+                                    <div className="grid gap-2">
+                                        <h1 className="lg:text-2xl ">About this gig</h1>
+                                        <p className="text-gray-600 md:text-lg">{gig.fullDescription}</p>
+                                    </div>
+                                    
+                                </div> 
+                                <div className="md:col-2 sticky grid content-center h-full ">
+                                    <div className="bg-gray-300 p-5 rounded-2xl">
+                                        <h1 className="lg:text-2xl">Abour Prices</h1>
+                                        <div className="ml-3">
+                                            <h1>The base price: {gig.basePrice} {gig.currency}</h1>
+                                            <h1>How price adding: {gig.priceType}</h1>
+                                        </div>
+                                    </div>
+                                </div>  
+                        </div>
+                        <div className="flex space-x-5 w-full items-center  my-10">
+                            <h1>Category: </h1>  
+                            <h1 className="bg-gray-200 rounded-xl px-4 py-1">{gig.category.name}</h1>
+                        </div>
+                        <div className="flex justify-end w-full">
+                            <button className="bg-black/85 rounded-md px-4 py-1 text-white active:scale-85"
+                            onClick={()=> setShowBookingForm(!showBookingForm)}
+                            >
+                                Book
+                            </button>
+                        </div>
                 </div>
-                 
-           </div>
-        </div>
+                </div>
+
+            </main>
+            {
+                showBookingForm && 
+                <BookingForm onClose={()=> setShowBookingForm(!showBookingForm)}
+                onSubmit={(bookingRequestDto: any)=>console.log(bookingRequestDto)}
+                />
+            }
+
+
+        </>
+        
+
     )
 }
