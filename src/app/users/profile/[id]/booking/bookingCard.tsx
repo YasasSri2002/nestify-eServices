3c1743e66c2dd93c1bf3510bb387d81044c8a226
YasasSri2002 <srimedagedara234@gmail.com>
2026-04-02 09:31:40 +0530

    fix: prevent reschedule and cancel actions for completed bookings

--- src/app/users/profile/[id]/booking/bookingCard.tsx
+++ src/app/users/profile/[id]/booking/bookingCard.tsx
@@ -159,7 +159,7 @@ export default function BookingCard({bookingData}:{bookingData:BookingResponseDt
             <div className="sm:col-span-5 grid gap-3">
                 <div className="grid gap-2">
                     <h1 className="text-lg xl:text-2xl capitalize">{bookingData.serviceGigResponseDto.title}</h1>
-                    <h2 className="text-gray-600 text-md xl:text-xl">with {bookingData.providerDto.firstName}</h2>
+                    <h2 className="text-gray-600 text-md xl:text-xl">with {bookingData.providerDto.userName}</h2>
                 </div>
                 <div className="flex gap-5">
                     <h1 className="flex gap-2 items-center"><DynamicIcon name="CiCalendar"/> {bookingData.startingDate}</h1>
@@ -171,29 +171,32 @@ export default function BookingCard({bookingData}:{bookingData:BookingResponseDt
                 <div className="flex justify-end gap-3 items-center">
                         {renderStatusTag()}
                 </div>
-                <div className="flex justify-end gap-3 flex-wrap">
-                    <button className="px-2 md:px-4 md:py-1  text-green-500 border border-green-500 rounded-md flex items-center gap-2 
-                        active:scale-75 active:bg-green-500 active:text-white "
-                        onClick={handleMarkAsComplete}
-                        title="Mark as complete"
+                {
+                    bookingData.status !== "completed" && 
+                    <div className="flex justify-end gap-3 flex-wrap">
+                        <button className="px-2 md:px-4 md:py-1  text-green-500 border border-green-500 rounded-md flex items-center gap-2 
+                            active:scale-75 active:bg-green-500 active:text-white "
+                            onClick={handleMarkAsComplete}
+                            title="Mark as complete"
+                            >
+                            <DynamicIcon name="IoCheckmarkDoneOutline"/>
+                        </button>
+                        <button className="px-2 md:px-4 md:py-1  text-blue-500 border border-blue-500 rounded-md flex items-center gap-2 
+                            active:scale-75 active:bg-blue-500 active:text-white"
+                            title="Reschedule"
+                            onClick={()=> setShowRescheduleForm(true)}
+                            >
+                        <DynamicIcon name="FaRegClock"/>
+                        </button>
+                        <button className="px-2 md:px-4 md:py-1 border   text-red-500 rounded-md flex items-center gap-2 active:scale-75
+                        active:bg-red-500 active:text-white"
+                        title="Cancel"
+                        onClick={handleCancelBooking}
                         >
-                        <DynamicIcon name="IoCheckmarkDoneOutline"/>
-                    </button>
-                    <button className="px-2 md:px-4 md:py-1  text-blue-500 border border-blue-500 rounded-md flex items-center gap-2 
-                        active:scale-75 active:bg-blue-500 active:text-white"
-                        title="Reschedule"
-                        onClick={()=> setShowRescheduleForm(true)}
-                        >
-                       <DynamicIcon name="FaRegClock"/>
-                    </button>
-                     <button className="px-2 md:px-4 md:py-1 border   text-red-500 rounded-md flex items-center gap-2 active:scale-75
-                     active:bg-red-500 active:text-white"
-                     title="Cancel"
-                     onClick={handleCancelBooking}
-                     >
-                       <DynamicIcon name="MdClose"/>
-                    </button>
-                </div>
+                        <DynamicIcon name="MdClose"/>
+                        </button>
+                    </div>
+                }
                 <div className="flex items-start justify-end">
                     <button className="text-blue-500 flex items-center gap-2"> View details <DynamicIcon name="FaArrowRight"/></button>
                 </div>
