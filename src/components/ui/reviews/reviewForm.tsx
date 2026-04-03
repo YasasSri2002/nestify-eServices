86351c16414dca4022d01756e498a84e983b6746
YasasSri2002 <srimedagedara234@gmail.com>
2026-04-03 18:59:26 +0530

    style: review form

--- src/components/ui/reviews/reviewForm.tsx
+++ src/components/ui/reviews/reviewForm.tsx
@@ -53,43 +53,52 @@ export default function ReviewForm({onClose,gigId,providersId}:{onClose:()=>void
    
    
     return(
-            <div className="grid gap-5 justify-items-center content-center w-dvw md:w-fit  bg-gray-200 px-5 py-10 rounded-2xl">
-                
-                <h1 className="md:text-2xl">Tell us how was your experience with Provider</h1>
-                <form onSubmit={submitForm} className="grid content-center gap-4">
-                    
-                    <div className="flex space-x-5 justify-center">
-                        {
-                            [1,2,3,4,5].map((_,i)=>{
-                             const starRate = i+1;
-                                return(
-                                <button  key={i} type="button" >
-                                    <DynamicIcon 
-                                     onClick={()=>getRateValue(i+1)} name="FaStar" 
-                                     className={`text-sm lg:text-4xl 
-                                                ${ starRate <= rating ? 'text-amber-400': 'text-gray-400'}`}
-                                    />
-                                </button>)
-                                
-                            })
-                        }
-                    </div>
-                    <div className="grid gap-2">
-                        <label htmlFor="message">Leave your thoughts</label>
-                        <textarea name="message" id="client-message" 
-                            className="w-60 sm:w-100 lg:w-120 h-30 sm:h-50 rounded-2xl px-2 pt-2 border focus:outline-0 resize-none bg-gray-100"></textarea>
+            <div className="flex justify-center items-center inset-0 z-50 fixed w-full "
+            onClick={onClose}>
 
-                    </div>
-
-                    <div className="flex justify-center items-center">
-                        <button type="submit" 
-                            className="rounded-xl bg-blue-600 px-5 py-1 text-white/85 hover:scale-105" >
-                            Submit
+                <div className="grid gap-5 justify-items-center content-center w-dvw max-w-4xl bg-gray-200 p-5 
+                 rounded-2xl max-h-[90vh] mx-4 sm:mx-0"
+                 onClick={(event)=> event.stopPropagation()}>
+                    <div className="flex justify-end w-full">
+                        <button onClick={onClose} >
+                            <DynamicIcon name="IoClose" className="w-5 h-5"/>
                         </button>
                     </div>
-                    
-                </form>
-                
+                    <h1 className="md:text-2xl">Tell us how was your experience with Provider</h1>
+                    <form onSubmit={submitForm} className="grid content-center gap-4">
+                        
+                        <div className="flex space-x-5 justify-center">
+                            {
+                                [1,2,3,4,5].map((_,i)=>{
+                                const starRate = i+1;
+                                    return(
+                                    <button  key={i} type="button" >
+                                        <DynamicIcon 
+                                        onClick={()=>getRateValue(i+1)} name="FaStar" 
+                                        className={`text-sm lg:text-4xl 
+                                                    ${ starRate <= rating ? 'text-amber-400': 'text-gray-400'}`}
+                                        />
+                                    </button>)
+                                    
+                                })
+                            }
+                        </div>
+                        <div className="grid gap-2">
+                            <label htmlFor="message">Leave your thoughts</label>
+                            <textarea name="message" id="client-message" 
+                                className="w-60 sm:w-100 lg:w-120 h-30 sm:h-50 rounded-2xl px-2 pt-2 border focus:outline-0 resize-none bg-gray-100"></textarea>
+
+                        </div>
+
+                        <div className="flex justify-center items-center">
+                            <button type="submit" 
+                                className="rounded-xl bg-blue-600 px-5 py-1 text-white/85 hover:scale-105" >
+                                Submit
+                            </button>
+                        </div>
+                        
+                    </form>
+                </div>
             </div>
     )
 }
